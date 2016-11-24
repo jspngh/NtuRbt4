3d21591dc0b7109d384c97e7baf82a08ae1e8d64
vdutor <dutordoirv@gmail.com>
2016-11-24 18:53:36 +0800

    filter contours

--- main.cpp
+++ main.cpp
@@ -16,18 +16,88 @@
 
 #include "image.hpp"
 
+#define NUM_TEMPLATES 3
+
 using namespace cv;
 using namespace std;
 
+// TODO: place in macros if necessary
 int thresh = 100;
 int max_thresh = 255;
-RNG rng(12345);
+RNG rng(0);
 int min_size = 15;
 
 
+void filter_contours(vector<vector<Point>> contours, int min_area, int min_distance = 10)
+{
+    for( int i = 0; i< contours.size(); i++ )
+    {
+        double area=contourArea( contours[i],false);
+        if(area<min_area)
+            contours.erase(contours.begin() + i);
+    }
+}
+
+
+vector<vector<Point>> get_contours(Mat gray)
+{
+    Mat canny_output;
+    vector<vector<Point>> contours;
+    vector<Vec4i> hierarchy;
+ 
+    /// Detect edges using canny
+    Canny( gray, canny_output, 50, 150, 3 );
+    /// Find contours
+    findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+    return contours;
+}
+
+void find_moments( Mat gray )
+{
+    vector<vector<Point> > contours = get_contours(gray);
+
+    /// Get the moments
+    vector<Moments> mu(contours.size() );
+    for( int i = 0; i < contours.size(); i++ )
+    { mu[i] = moments( contours[i], false ); }
+ 
+    ///  Get the mass centers:
+    vector<Point2f> mc( contours.size() );
+    for( int i = 0; i < contours.size(); i++ )
+    { mc[i] = Point2f( mu[i].m10/mu[i].m00 , mu[i].m01/mu[i].m00 ); }
+ 
+    /// Draw contours
+    Mat drawing = Mat::zeros( gray.size(), CV_8UC3 );
+    for( int i = 0; i< contours.size(); i++ )
+    {
+        Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+        drawContours( drawing, contours, i, color);
+        circle( drawing, mc[i], 4, color, -1, 8, 0 );
+    }
+ 
+    /// Show in a window
+    namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
+    imshow( "Contours", drawing );
+    waitKey(0);
+ 
+    /// Calculate the area with the moments 00 and compare with the result of the OpenCV function
+    printf("\t Info: Area and Contour Length \n");
+    for( int i = 0; i< contours.size(); i++ )
+        printf(" * Contour[%d] - Area (M_00) = %.2f - Area OpenCV: %.2f - Length: %.2f \n", i, mu[i].m00, contourArea(contours[i]), arcLength( contours[i], true ) );
+ 
+}
+
 void read_templates()
 {
-    path
+    string path = "templates/";
+    for (int i=1; i<=NUM_TEMPLATES; i++)
+    {
+        string templ_file = path + "template" + to_string(i) + ".png";
+        Mat templ_im = imread(templ_file);
+        Mat frame_bw;
+        cvtColor(templ_im, frame_bw, COLOR_BGR2GRAY);
+        find_moments(frame_bw);
+    }
 
 }
 
@@ -42,18 +112,14 @@ void canny(Mat src_gray)
     /// Find contours
     findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
 
+    filter_contours(contours, min_size);
+
     /// Draw contours
     Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
     for( int i = 0; i< contours.size(); i++ )
     {
-        float size = contourArea(contours.at(i));
-        Scalar color;
-        if (size > min_size)
-        {
-            color = Scalar(255,0,0);
-            drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
-            cout << size << endl;
-        }
+        Scalar color = Scalar(255,0,0);
+        drawContours( drawing, contours, i, color);
     }
 
     /// Show in a window
@@ -102,6 +168,8 @@ void process_image(Mat frame)
 
 int main(int, char**)
 {
+    //read_templates();
+
     for(int i=1; i <= 8; i++)
     {
         string path = "res/calib" + to_string(i) + ".png";
